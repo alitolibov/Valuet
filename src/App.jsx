d4d605ad0a83042773fdae50a1cc1984ad806609
0Amin3 <aminfedya2888@gmail.com>
2023-03-18 20:29:08 +0500

    v0.1

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Aside from "./components/Aside"
 import Header from "./components/Header"
 import Layout from "./Layout/Layout"
+import Overview from "./pages/Overview"
 
 
 function App() {
@@ -10,10 +11,10 @@ function App() {
     <div className="w-full h-[100vh] bg-bg ">
       <Routes>
         <Route path="/" element={ <Layout/>}>
-            <Route path="/" index element={ <center><h1>Overview</h1></center>}/>
-            <Route path="wallets" index element={ <center><h1>Wallets</h1></center>}/>
-            <Route path="transictions" index element={ <center><h1>Transictions</h1></center>}/>
-            <Route path="exchange" index element={ <center><h1>Exchange</h1></center>}/>
+            <Route path="/" index element={ <Overview/> }/>
+            <Route path="wallets"  element={ <center><h1>Wallets</h1></center>}/>
+            <Route path="transictions"  element={ <center><h1>Transictions</h1></center>}/>
+            <Route path="exchange"  element={ <center><h1>Exchange</h1></center>}/>
             <Route path="market" index element={ <center><h1>Market</h1></center>}/>
         </Route>
       </Routes>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,8 @@ module.exports = {
       backgroundImage: {
         gradient: ['linear-gradient(0.32deg, rgba(28, 35, 64, 0) -5.79%, #1C0F54 114.59%)'],
         bg: ['radial-gradient(46.41% 73.99% at 46.63% 48.05%, #20135C 0%, #140739 92.82%)'],
-        textBg: ['linear-gradient(271.88deg, #3887FE 4.26%, #3BA0FF 51.37%, #5FB2FF 99.01%)']
+        textBg: ['linear-gradient(271.88deg, #3887FE 4.26%, #3BA0FF 51.37%, #5FB2FF 99.01%)'],
+        balance:['linear-gradient(180deg, rgba(27, 18, 78, 0.2) 0%, #0F0B38 93.37%)']
       },
       dropShadow: {
         aside: ['0px -5px 25px rgba(0, 3, 32, 0.5)']
