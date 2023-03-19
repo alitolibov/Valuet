8ca6289736906929fb5ddb6f3cfbbf62ac17f292
alitolibov <alitolibov066@gmail.com>
2023-03-19 19:19:17 +0500

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
+import { lazy, Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
+import Loading from "./components/Loading"
 import Layout from "./Layout/Layout"
-import Wallets from "./page/Wallets"
-import Overview from "./pages/Overview"
-import Login from "./pages/Login"
+
+const Overview = lazy(() => import("./pages/Overview"))
+const Wallets = lazy(() => import("./page/Wallets"))
+const Login = lazy(() => import("./pages/Login"))
+
+
 
 function App() {
 
   return (
     <div className="w-full h-[100vh] bg-bg ">
+      <Suspense fallback={<Loading/>}>
       <Routes>
         <Route path="sign" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="/" index element={ <Overview/> } />
-          <Route path="wallets" index element={<Wallets />} />
-          <Route path="transictions" index element={<center><h1>Transictions</h1></center>} />
-          <Route path="exchange" index element={<center><h1>Exchange</h1></center>} />
-          <Route path="market" index element={<center><h1>Market</h1></center>} />
+          <Route path="wallets" element={<Wallets />} />
+          <Route path="transictions" element={<center><h1>Transictions</h1></center>} />
+          <Route path="exchange" element={<center><h1>Exchange</h1></center>} />
+          <Route path="market" element={<center><h1>Market</h1></center>} />
         </Route>
       </Routes>
+      </Suspense>
     </div>
   )
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,7 @@
+const Loading = () => {
+   return ( 
+      <div className="loader"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>
+    );
+}
+ 
+export default Loading;
