cd5219232fac93c58fbc15f2f38bdb42b1d12729
0Amin3 <aminfedya2888@gmail.com>
2023-03-22 18:08:36 +0500

    vv

--- src/components/MyPieChart.jsx
+++ src/components/MyPieChart.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -8,23 +8,20 @@ export const data = {
 	labels: [],
 	datasets: [
 		{
-			label: "# of Votes",
-			data: [12, 19, 3, 5, 2, 3],
+			label: "",
+			coin: ['Ethereum', 'Bitcoin', 'Dash'],
+			data: [18, 64, 19],
 			backgroundColor: [
-				"rgba(255, 99, 132, 0.2)",
-				"rgba(54, 162, 235, 0.2)",
-				"rgba(255, 206, 86, 0.2)",
-				"rgba(75, 192, 192, 0.2)",
-				"rgba(153, 102, 255, 0.2)",
-				"rgba(255, 159, 64, 0.2)",
+				"rgba(255, 206, 86, 1)",
+
+				"rgba(54, 162, 235, 1)",
+				"#ffffff",
 			],
 			borderColor: [
-				"rgba(255, 99, 132, 1)",
-				"rgba(54, 162, 235, 1)",
 				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
-				"rgba(255, 159, 64, 1)",
+				"rgba(54, 162, 235, 1)",
+				"#ffffff",
+
 			],
 			borderWidth: 1,
 		},
@@ -32,5 +29,5 @@ export const data = {
 };
 
 export function MyPieChart() {
-	return <Pie data={data} />;
+	return <Doughnut data={data} />;
 }
